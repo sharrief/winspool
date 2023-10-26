1759d361e2e55419a5dfba58f8fce92f2f1ce754
Sharrief Shabazz <sharrief@qyutech.com>
2023-10-26 10:34:16 -0700

    more scaffoldings

---
+++ environment.d.ts
@@ -0,0 +1,17 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      POSTGRES_PASSWORD: string
+      POSTGRES_USER: string
+      POSTGRES_DB: string
+      POSTGRES_HOST: string
+
+      PGHOST: string
+      PGUSER: string
+      PGPASSWORD: string
+      PGDATABASE: string
+    }
+  }
+}
+
+export {};
