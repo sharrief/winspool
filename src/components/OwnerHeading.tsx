9b175f6f894962528e9636346896731fe06f6d31
Sharrief Shabazz <me@sharrief.com>
2023-10-27 11:13:12 -0700

    styling adjustments

--- src/components/OwnerHeading.tsx
+++ src/components/OwnerHeading.tsx
@@ -7,8 +7,8 @@ export interface OwnerHeadingProps {
 
 export default function OwnerHeading({ place = 999, name = 'N/A', score = 0 }: OwnerHeadingProps) {
   return <div className="py-5 border-b-2 border-neutral-500 drop-shadow-xl flex flex-row w-full">
-    <span className='text-2xl lg:text-5xl pe-2'>#{place}</span>
-    <span className='text-2xl lg:text-5xl flex-auto'>{name}</span>
-    <span className='text-2xl lg:text-5xl'>{score} pts</span>
+    <span className='drop-shadow-md text-2xl lg:text-5xl pe-2'>#<span>{place}</span></span>
+    <span className='drop-shadow-md text-2xl lg:text-5xl flex-auto'>{name}</span>
+    <span className='drop-shadow-md text-2xl lg:text-5xl'>{score} pts</span>
   </div>
 }
--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -24,7 +24,7 @@ export default function TeamCard({
   name, image, theme,
   wins, losses, ties, score
 }: TeamCardProps) {
-  return (<div className="rounded-xl shadow-xl" 
+  return (<div className="rounded-xl shadow-xl shadow-neutral-400" 
   style={{ backgroundColor: theme.secondaryColor}}>
   <div 
     className="p-2 rounded-t-xl shadow-xl"
@@ -52,9 +52,9 @@ export default function TeamCard({
   <div 
   className={`px-2 py-11 rounded-b-xl ${theme.secondaryText === 'black' ? 'text-black':'text-white'}`}
   >
-    <div className="w-full text-center text-lg xl:text-xl">{name}</div>
-    <div className="w-full text-center text-lg xl:text-xl">{wins} -{ties ? ` ${ties} -` : ''} {losses}</div>
-    <div className="w-full text-center text-lg xl:text-xl">{score} pts</div>
+    <div className="w-full text-center text-lg sm:text-xl">{name}</div>
+    <div className="w-full text-center text-lg sm:text-2xl">{wins} -{ties ? ` ${ties} -` : ''} {losses}</div>
+    <div className="w-full text-center text-lg sm:text-3xl">{score} pts</div>
   </div>
 </div>);
 }
